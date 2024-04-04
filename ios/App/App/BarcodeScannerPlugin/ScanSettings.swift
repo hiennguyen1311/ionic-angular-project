dd0500ce1723630c222434a2dd22fe9628e797ab
Thinh Ngo <thinh.ngo@hitachivantara.com>
2024-04-04 13:53:14 +0700

    Adding iOS project

---
+++ ios/App/App/BarcodeScannerPlugin/ScanSettings.swift
@@ -0,0 +1,9 @@
+import Foundation
+import MLKitBarcodeScanning
+import AVFoundation
+
+@objc public class ScanSettings: NSObject {
+    public var showUIElements: Bool = false
+    public var formats: [BarcodeFormat] = []
+    public var lensFacing: AVCaptureDevice.Position = .back
+}
